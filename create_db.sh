fa50b9062a777e97790e2d2a68b96321afcb01c7
Gabriel Boehme <gabboehme@gmail.com>
2023-12-05 15:26:32 -0300

    feat: added create db script  (docker)

--- create_db.sh
+++
@@ -1,8 +0,0 @@
-docker run\
-    -d\
-    -p 5431:5432\
-    --name skooter-postgres\
-    -e POSTGRES_DB=skooterdb\
-    -e POSTGRES_USER=admin\
-    -e POSTGRES_PASSWORD=skooteradmin\
-    postgres:latest
